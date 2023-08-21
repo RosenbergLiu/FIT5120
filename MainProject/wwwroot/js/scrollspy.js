b3d799962b208296fdf20f128810cee28f905200
Rosenberg Xiangyu Liu <xygenio@hotmail.com>
2023-08-21 19:17:35 +1000

    Move customized Js code to js folder

---
+++ MainProject/wwwroot/js/scrollspy.js
@@ -0,0 +1,22 @@
+﻿window.addEventListener('scroll', function () {
+    var navbarHeight = 70;
+    var scrollPosition = window.scrollY + navbarHeight;
+    var home = document.getElementById('home').offsetTop;
+    var form = document.getElementById('form').offsetTop;
+    var visualization = document.getElementById('visualization').offsetTop;
+    var about = document.getElementById('about').offsetTop;
+
+    setActiveLink('home', scrollPosition >= home && scrollPosition < form);
+    setActiveLink('form', scrollPosition >= form && scrollPosition < visualization);
+    setActiveLink('visualization', scrollPosition >= visualization && scrollPosition < about);
+    setActiveLink('about', scrollPosition >= about);
+});
+
+function setActiveLink(sectionId, isActive) {
+    var link = document.querySelector('.nav-' + sectionId);
+    if (isActive) {
+        link.classList.add('active');
+    } else {
+        link.classList.remove('active');
+    }
+}
---
+++ MainProject/wwwroot/js/scrollto.js
@@ -0,0 +1,31 @@
+﻿function scrollToHome() {
+    var element = document.getElementById("home");
+    if (element) {
+        var position = element.getBoundingClientRect().top + window.pageYOffset - 70;
+        window.scrollTo({ top: position, behavior: 'smooth' });
+    }
+}
+
+function scrollToForm() {
+    var element = document.getElementById("form");
+    if (element) {
+        var position = element.getBoundingClientRect().top + window.pageYOffset - 70;
+        window.scrollTo({ top: position, behavior: 'smooth' });
+    }
+}
+
+function scrollToVisualization() {
+    var element = document.getElementById("visualization");
+    if (element) {
+        var position = element.getBoundingClientRect().top + window.pageYOffset - 70;
+        window.scrollTo({ top: position, behavior: 'smooth' });
+    }
+}
+
+function scrollToAboutUs() {
+    var element = document.getElementById("about");
+    if (element) {
+        var position = element.getBoundingClientRect().top + window.pageYOffset - 70;
+        window.scrollTo({ top: position, behavior: 'smooth' });
+    }
+}
