30f5a1ffc3151ce7319b1bc84bd421cfe0e6f580
Tien Nguyen <xygenio@hotmail.com>
2023-08-25 15:42:04 +1000

    OnSubmit is not necessary

--- MainProject/Pages/Index.razor.cs
+++ MainProject/Pages/Index.razor.cs
@@ -13,18 +13,9 @@ namespace MainProject.Pages
             await InitialFormAsync();
         }
 
-        public async Task OnSubmit(FoodFormModel args)
-        {
-
-        }
-
         void DeleteItem(SavedFood item)
         {
-            // Logic to delete the item from savedFoodList
             savedFoodList.Remove(item);
-            // You may also need to call any database or service to make the deletion persistent
-
-            // Refresh the grid or necessary components
             StateHasChanged();
         }
     }
