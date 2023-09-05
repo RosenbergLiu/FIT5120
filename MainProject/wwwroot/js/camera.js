69391724a9772bf28e1f8305959f69e9cafb181f
Rosenberg Xiangyu Liu <xygenio@hotmail.com>
2023-09-05 21:19:58 +1000

    Use camera

---
+++ MainProject/wwwroot/js/camera.js
@@ -0,0 +1,25 @@
+﻿async function initializeCamera(videoElementId) {
+    const videoElement = document.getElementById(videoElementId);
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            videoElement.srcObject = stream;
+            return true;
+        } catch (err) {
+            console.error("An error occurred: " + err);
+            return false;
+        }
+    }
+    return false;
+}
+
+function captureImage(videoElementId, canvasElementId) {
+    const videoElement = document.getElementById(videoElementId);
+    const canvasElement = document.getElementById(canvasElementId);
+
+    const context = canvasElement.getContext('2d');
+    context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+
+    return canvasElement.toDataURL('image/png');
+}
---
+++ MainProject/wwwroot/js/saveas.js
@@ -0,0 +1,9 @@
+﻿function downloadICS(icsContent, fileName) {
+    const blob = new Blob([icsContent], { type: 'text/calendar' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    URL.revokeObjectURL(url);
+}
