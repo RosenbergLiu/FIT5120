c3e3160f807337f85d34a2375b353cb67381064f
Rosenberg Xiangyu Liu <xygenio@hotmail.com>
2023-10-06 21:20:08 +1100

    using ajax

--- FoodSaver/wwwroot/js/site.js
+++ FoodSaver/wwwroot/js/site.js
@@ -3,27 +3,34 @@
 
 // Write your JavaScript code.
 
-$(document).ready(function () {
-    $('#refreshProductsBtn').on('click', function (e) {
-        e.preventDefault();
-        $.get("/Index?handler=RefreshProducts", function (data) {
-            var content = '';
-            for (var i = 0; i < data.length; i++) {
-                content += `
-                    <div class="col-lg-3 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-wrap">
-                            <img src="${data[i].url}" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>${data[i].name}</h4>
-                                <p>${data[i].qty}</p>
-                                <div class="portfolio-links">
-                                    <a><i class="bx bx-plus"></i> Add</a>
-                                </div>
-                            </div>
+function displayRandomProducts() {
+    let displayedProducts = [];
+    let productContainer = document.getElementById('productContainer');
+
+    // Clear the previous products
+    productContainer.innerHTML = "";
+    // Get 4 random products
+    while (displayedProducts.length < 4) {
+        let randomIndex = Math.floor(Math.random() * allProducts.length);
+        if (displayedProducts.indexOf(randomIndex) === -1) {
+            displayedProducts.push(randomIndex);
+            let product = allProducts[randomIndex];
+
+            let productDiv = `
+            <div class="col-lg-3 col-md-6 portfolio-item filter-app">
+                <div class="portfolio-wrap">
+                    <img src="${product.url}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>${product.name}</h4>
+                        <p>${product.qty}</p>
+                        <div class="portfolio-links">
+                            <a><i class="bx bx-plus"></i> Add</a>
                         </div>
-                    </div>`;
-            }
-            $('.portfolio-container').html(content);
-        });
-    });
-});
+                    </div>
+                </div>
+            </div>`;
+
+            productContainer.innerHTML += productDiv;
+        }
+    }
+}
