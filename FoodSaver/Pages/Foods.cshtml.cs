8c7c668eb926f4e35a5a138c48a59393099f9fbe
Rosenberg Xiangyu Liu <xygenio@hotmail.com>
2023-10-13 15:19:35 +1100

    change title section

---
+++ FoodSaver/Pages/Foods.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodSaver.Pages
+{
+    public class FoodsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FoodSaver/Pages/WasteSource.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodSaver.Pages
+{
+    public class WasteByModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
